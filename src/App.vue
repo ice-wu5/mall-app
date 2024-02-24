a60cb82079ea449c6f8f7c13315c0a00ea1cac84
ice_wu <xuhong267@gmail.com>
2024-02-24 19:25:47 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <nav>
       <router-link to="/">test</router-link> |
-      <router-link to="/about">test</router-link>
+      <router-link to="/about">test333</router-link>
     </nav>
     <router-view/>
   </div>
